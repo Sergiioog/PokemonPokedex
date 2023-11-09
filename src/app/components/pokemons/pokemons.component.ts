dd44c3a859037a44f5f3b10b367a5846fdab144d
Sergiioog <sergiogargor9@gmail.com>
2023-11-09 18:29:37 +0100

    TS de pokemons

--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemons.component.css']
 })
 export class PokemonsComponent {
+  
+  pokemons = [
+    { imagen: "assets/pokemons/Bulbasur.png", descripcionImg:"Bulbasur img",
+      nombre: 'Bulbasur', altura: '0,7m' , peso: '6,9kg', categoria: 'Semilla',tipo: 'Planta',
+    },
+    { imagen: "assets/pokemons/Ivysaur.png", descripcionImg:"Ivysaur img",
+    nombre: 'Ivysaur', altura: '1,0m' , peso: '13,0kg', categoria: 'Semilla',tipo: 'Planta'
+    },
+    { imagen: "assets/pokemons/Venusaur.png", descripcionImg:"Venusaur img",
+    nombre: 'Venusaur', altura: '2,0m' , peso: '100,0 kg', categoria: 'Semilla',tipo: 'Planta'
+    },
+    { imagen: "assets/pokemons/Bulbasur.png", descripcionImg:"Bulbasur img",
+    nombre: 'Bulbasur', altura: '0,7m' , peso: '6,9kg', categoria: 'Semilla',tipo: 'Planta'
+    },
 
+  ];
 }
